dab4e4db3dd7d21fd7174dc119c139807c36ea90
Mdlalose4life <blessing1059@gmail.com>
2023-07-06 17:41:00 +0200

    2-floor.py

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import math
 
-def floor(n: float) -> float:
+
+def floor(n: float) -> int:
     """
     type-annotated function floor which takes a float n as
     argument and returns the floor of the float.
