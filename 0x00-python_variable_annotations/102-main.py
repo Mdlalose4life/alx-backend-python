2e49b3ce92cbcc6d791bd1f7a629078a57552865
Mdlalose4life <blessing1059@gmail.com>
2023-07-06 21:58:09 +0200

    102-type_checking.py

--- 0x00-python_variable_annotations/102-main.py
+++ 0x00-python_variable_annotations/102-main.py
@@ -1,22 +1,5 @@
 #!/usr/bin/env python3
-"""
-Use mypy to validate the following piece of code
-and apply any necessary changes.
-"""
 
-from typing import Tuple, List, Any
+zoom_array =  __import__('102-type_checking').zoom_array
 
-
-def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    zoomed_in: List = [
-        item for item in lst
-        for i in range(factor)
-    ]
-    return zoomed_in
-
-
-array = (12, 72, 91)
-
-zoom_2x = zoom_array(array)
-
-zoom_3x = zoom_array(array, 3)
+print(zoom_array.__annotations__)
