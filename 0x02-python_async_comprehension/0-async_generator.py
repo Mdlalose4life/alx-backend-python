cfce6b2f60e28dcd12305f9638b1d03547299f14
Mdlalose4life <blessing1059@gmail.com>
2023-07-11 23:18:13 +0200

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,9 +7,10 @@ Use the random module.
 """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> None:
+async def async_generator() -> Generator[float, None, None]:
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.random() * 10
