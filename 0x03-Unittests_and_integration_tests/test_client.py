e292e03aadc18c981feac57879d21eae7810e46e
Mdlalose4life <blessing1059@gmail.com>
2023-08-01 00:11:23 +0200

    add

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+"""
+import unittest
+from unittest.mock import Mock, patch
+from typing import Dict
+from parameterized import parameterized, parameterized_class
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    """
